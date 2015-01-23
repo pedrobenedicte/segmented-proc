c1b4f33c868419dadfed615351cf1f308869b6de
Pedro Benedicte <pedro.benedicte@est.fib.upc.edu>
2015-01-23 12:49:10 +0100

    Data cache not finished yet (almost).

--- src/stage_cache/cache_d.vhd
+++ src/stage_cache/cache_d.vhd
@@ -6,87 +6,89 @@ use std.textio.all;
 
 entity cache_d is
 	port (
-		clk				: in std_logic;
-		boot			: in std_logic;
-		we				: in std_logic;
-		add_logical		: in std_logic_vector(15 downto 0);
-		hit_miss		: out std_logic;
-		add_physical	: out std_logic_vector(15 downto 0)
+		clk				: in std_logic;		-- clock
+		boot				: in std_logic;		-- boot
+		enable			: in std_logic;		-- do something or not
+		r_w				: in std_logic;		-- read or write
+		cache_mem		: in std_logic;		-- access cache or memory
+		b_w				: in std_logic;		-- byte access or word access
+		add_physical	: in std_logic_vector(15 downto 0);
+		
+		memory_request	: out std_logic;
+		memory_r_w		: out std_logic;
+		memory_address	: out std_logic_vector(12 downto 0);
+		memory_in		: in std_logic_vector(63 downto 0);
+		memory_out		: out std_logic_vector(63 downto 0);
+		
+		data_out			: out std_logic_vector(15 downto 0)
 	);
 end entity;
 
-architecture Structure of tlb_d is
-	signal page_logical		: std_logic_vector(9 downto 0);
-	signal page_physical	: std_logic_vector(9 downto 0);
-	signal hit				: std_logic_vector(4 downto 0);
+architecture Structure of cache_d is
+	signal page		: std_logical_vector(9 downto 0);
+	signal index	: std_logical_vector(2 downto 0);
+	signal offset	: std_logical_vector(2 downto 0);
 	
 	-- 8 lines, 64b each line
-	-- 512b tlb size
-	type tlb_table is array (4 downto 0) of std_logic_vector(20 downto 0);
-	signal tlb : tlb_table;
+	-- 512b cache size
+	type cache_table is array (7 downto 0) of std_logic_vector(63 downto 0);
+	signal cache : cache_table;
 
-	-- Initialize TLB from file tlb_d.txt
-	procedure Load_TLB_Data (signal data : inout tlb_table) is
+	-- Initialize Cache_d from file cache_d.txt
+	procedure Load_Cache_Data (signal data : inout cache_table) is
 		-- Open File in Read Mode
-		file tlb_file	:text open read_mode is "tlb_d.txt";
+		file cache_file	:text open read_mode is "cache_d.txt";
 		variable lbuf	:line;
 		variable i		:integer := 0;
 		variable fdata	:std_logic_vector (20 downto 0);
 	begin
-		while not endfile(tlb_file) loop
+		while not endfile(cache_file) loop
 			-- read data from input file
-			readline(tlb_file, lbuf);
+			readline(cache_file, lbuf);
 			read(lbuf, fdata);
 			data(i) <= fdata;
 			i := i+1;
 		end loop;
 	end procedure;
 	
-	-- Check if a TLB entry has the page we are looking for
-	procedure Check_TLB_Entry (	signal valid	: in std_logic;
-								signal page		: in std_logic_vector(9 downto 0);
-								signal data		: in std_logic_vector(20 downto 0);
-								signal hit		: out std_logic) is
-	begin
-		if ((page = data(19 downto 10)) and valid = '1') then
-			hit <= '1';
-		else
-			hit <= '0';
-		end if;
-	end procedure;
-		
 begin
-	page_logical <= add_logical(15 downto 6);
-	add_physical <= page_physical(9 downto 0) & add_logical(5 downto 0);
+	page		<= add_physical(15 downto 6);
+	index		<= add_physical(5 downto 3);
+	offset	<= add_physical(2 downto 0);
 	
 	process (clk)
 	begin
-		if (clk'event and clk = '1') then
+		if (clk'event and clk = '0') then
 			if (boot = '1') then
-				Load_TLB_Data(tlb);
+				Load_Cache_Data(cache);
 			else
-				if (we = '1') then
-					Check_TLB_Entry(tlb(0)(20), page_logical, tlb(0), hit(0));
-					Check_TLB_Entry(tlb(1)(20), page_logical, tlb(1), hit(1));
-					Check_TLB_Entry(tlb(2)(20), page_logical, tlb(2), hit(2));
-					Check_TLB_Entry(tlb(3)(20), page_logical, tlb(3), hit(3));
-					Check_TLB_Entry(tlb(4)(20), page_logical, tlb(4), hit(4));
-					hit_miss <= hit(0) or hit(1) or hit(2) or hit(3) or hit(4);
-					if (hit(0) = '1') then
-						page_physical <= tlb(0)(9 downto 0);
-					elsif (hit(1) = '1') then
-						page_physical <= tlb(1)(9 downto 0);
-					elsif (hit(2) = '1') then
-						page_physical <= tlb(2)(9 downto 0);
-					elsif (hit(3) = '1') then
-						page_physical <= tlb(3)(9 downto 0);
-					elsif (hit(4) = '1') then
-						page_physical <= tlb(4)(9 downto 0);
+				if (enable = '1') then
+					if (r_w = '1') then
+						if (cache_mem = '1') then			-- load from cache
+							memory_request	<= '0';
+							
+						else										-- load from memory
+							memory_request	<= '1';
+							memory_r_w		<= '1';
+							memory_address	<= page & index;
+							cache()()		<= memory_in;
+						end if;
 					else
-						page_physical <= "0000000000";
+						if (cache_mem = '1') then			-- store to cache
+							memory_request <= '0';
+							
+							
+						else										-- store to memory
+							memory_request	<= '1';
+							memory_r_w		<= '0';
+							memory_address	<= page & index;
+							memory_out		<= cache()();
+						end if;
 					end if;
 				end if;
 			end if;
+		else
+			memory_request <= '0';
 		end if;
 	end process;
 			
