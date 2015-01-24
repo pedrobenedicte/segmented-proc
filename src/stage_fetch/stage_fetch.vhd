a664afb6370443ba2e97f41d7b3bcbd950f9c16c
Pedro Benedicte <pedro.benedicte@est.fib.upc.edu>
2015-01-24 23:32:00 +0100

    Fix.

--- src/stage_fetch/stage_fetch.vhd
+++ src/stage_fetch/stage_fetch.vhd
@@ -22,6 +22,7 @@ entity stage_fetch is
 		hit_miss		: out	std_logic;
 		
 		-- Physical addres obtained in previous miss
+		real_address	: out	std_logic_vector(15 downto 0);
 		memory_pc		: in	std_logic_vector(15 downto 0);
 		
 		-- no flipflop, pc comes from a flipflop
@@ -108,7 +109,7 @@ begin
 	
 	fetch_exception <= not tlb_hit;
 	
-	ir 				<= addess_tlb;
+	real_address	<= addess_tlb;
 	
 	imem_addr(2 downto 0) <= "000";
 	
